896dbcc8db7f763dedca40b3df502a230daa7c05
Timothy Olsen <timothyrobertolsen@gmail.com>
2025-01-14 20:52:04 -0800

    Cleaning up the console behavior for the user

--- DailyWordDefinition.py
+++ DailyWordDefinition.py
@@ -5,15 +5,13 @@ import platform
 import os
 
 def runProgram():
-    print('\nDaily_Word_Definition.exe running')
-    time.sleep(1)    
-    exeFilePath = get_exe_path('StartStopEditProgram')    
-    time.sleep(1)
-    print(f'\nRunning this exe using subprocess now:{exeFilePath}')    
+    print('\nLoading program now ', end="")    
+    exeFilePath = get_exe_path('StartStopEditProgram')        
     dep = Depdenencies(platform, getBaseDir, os, subprocess)
     RunExe(exeFilePath, dep)  
-    print('\nStarting application...')
-    time.sleep(1)  
+    for i in range(3):
+        print(". ", end="", flush=True)
+        time.sleep(1)  
 
 if __name__ == "__main__":    
     runProgram()
